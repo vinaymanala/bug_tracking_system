132d11b4fb37d49a798018ad2ec7c27ab6605970
Vinay Manala <62110273+manalavi@users.noreply.github.com>
2023-04-09 11:48:00 +0530

    Update posts.php

--- includes/posts.php
+++ includes/posts.php
@@ -1,25 +1,25 @@
 <?php 
-          //$db = $_SESSION['datab'];
-	//$con = mysqli_connect("localhost","root","",$db);
-	include_once('dbh.inc.php');
+          $db = $_SESSION['datab'];
+	$con = mysqli_connect("localhost","root","",$db);
+// 	include_once('dbh.inc.php');
       	if ($_SERVER['REQUEST_METHOD']=='POST' && empty($_SESSION['post-create']))
       	{	
       		$_SESSION["post-create"] == 'true';
-      		if ($conn ->connect_error)
+      		if ($con ->connect_error)
       		{
-      			echo "Failed to connect to DB".$conn->connect_error;
+      			echo "Failed to connect to DB".$con->connect_error;
       		}
       		$id = $_SESSION["id"];
-      		$imageName = $conn -> real_escape_string($_FILES["uploadFile"]["name"]);
-      		$imageType = $conn -> real_escape_string($_FILES["uploadFile"]["type"]);
-      		$imageData = $conn ->real_escape_string(file_get_contents($_FILES["uploadFile"]["tmp_name"]));
+      		$imageName = $con -> real_escape_string($_FILES["uploadFile"]["name"]);
+      		$imageType = $con -> real_escape_string($_FILES["uploadFile"]["type"]);
+      		$imageData = $con ->real_escape_string(file_get_contents($_FILES["uploadFile"]["tmp_name"]));
       		
-      		$title = $conn -> real_escape_string($_POST["post-title"]);
-      		$status = $conn -> real_escape_string($_POST["post-status"]);
+      		$title = $con -> real_escape_string($_POST["post-title"]);
+      		$status = $con -> real_escape_string($_POST["post-status"]);
       		if (substr($imageType,0,5) == "image" and !empty($title) and !empty($imageData) and !empty($status))
       		{
       			$query = "INSERT INTO posts_details(posts_status,posts_user,posts_image,image_ext) VALUES('$status','$id','$imageData','$imageType');";
-      			mysqli_query($conn,$query);
+      			mysqli_query($con,$query);
       			header("location: ../index.php?create-post=success");
       		}else
       		{
